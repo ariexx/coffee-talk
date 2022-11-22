1c94fed93e3abc524357854c3fccdb02827aabdc
Muhammad Arief <mgi.official1@gmail.com>
2022-11-22 23:35:10 +0700

    admin can add user, add polciy

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+                    ->rules(['required', 'max:255']),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->rules(['required', 'email', 'max:255']),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->rules(['required', 'min:8', 'max:255'])
+                    ->dehydrateStateUsing(fn($state) => Hash::make($state)),
+                Forms\Components\Select::make('role')
+                    ->options([
+                        'employee' => 'Employee',
+                    ])
+                    ->required()
+                    ->rules(['required', 'in:employee']),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('role')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageUsers::route('/'),
+        ];
+    }
+}
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+	use HandlesAuthorization;
+
+	public function viewAny(User $user): bool
+	{
+		return $user->role === 'admin';
+	}
+
+	public function view(User $user): bool
+	{
+        return $user->role === 'admin';
+	}
+
+	public function create(User $user): bool
+	{
+        return $user->role === 'admin';
+	}
+
+	public function update(User $user): bool
+	{
+        return $user->role === 'admin';
+	}
+
+	public function delete(User $user): bool
+	{
+        return $user->role === 'admin';
+	}
+
+}
